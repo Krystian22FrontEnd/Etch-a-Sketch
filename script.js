78b1a1f8c8ee1c4491eb7c1ed9d116dde1634407
Krystian Krupinski <krupinskikrystian01@gmail.com>
2025-02-14 15:32:55 +0100

    in getDynamicGrid fn moved createElement to for loop and added new variable which adjusts size of squares inside of grid based on how big or small you want your grid to be

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-function getDynamicGrid() {
+function getDynamicGrid(size) {
 const div = document.querySelector(".container")
 
-const grid = document.createElement("div");
+div.innerHTML = ""
 
-grid.classList.add("containerElement");
+for (let i = 0; i < size * size; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("containerElement");
+    div.appendChild(grid)
 
-div.appendChild(grid)
+    let gridSize = 960 / size
+
+    grid.style.width = `${gridSize}px`
+    grid.style.height = `${gridSize}px`
 }
 
-for (let i = 0; i < 16 * 16; i++) {
-    getDynamicGrid()
 }
+getDynamicGrid(16)
 
 function getPrompt() {
     const aNumber = Number(window.prompt("Type a number", ""));
 
-    for (let i = 0; i < aNumber; i++) {
-        getDynamicGrid()
-    }
-
     if(isNaN(aNumber)) {
         alert("PLEASE ENTER A NUMBER")
-    } else if(aNumber > 101) {
+    } else if(aNumber > 100) {
         alert("YOU CAN ENTER A MAX VALUE OF 100")
     }
+    getDynamicGrid(aNumber)
 }
